ee574b52a59d50b76fdd06c9ac7ac3686999ee7b
fariq <tonykriss2323@gmail.com>
2025-06-16 16:18:19 +0700

    k

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Like;
+use App\Models\Comment;
+use Illuminate\View\View;
 
 class PostController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $posts = Post::latest()->paginate(30);
+
+        return view('posts.index', compact('posts'));
+    }
 }
